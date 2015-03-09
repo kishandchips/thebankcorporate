a6b8f8b1cab8e7e0cc3cebc1a4c548ad502874bc
franszo <josh@kishandchips.com>
2015-03-09 15:46:52 +0000

    socials, styles
    Signed-off-by: franszo <josh@kishandchips.com>

---
+++ inc/default/shortcodes.php
@@ -0,0 +1,13 @@
+<?php 
+function socials_func( ) {
+
+	$output = '<div class="socials">';
+	$output .= '<a href="http://www.facebook.com/pages/The-Bank/277573288939381" target="_blank" class="icon icon-facebook"></a>';
+	$output .= '<a href="http://twitter.com/#!/TheBankAgency" class="icon icon-twitter" target="_blank"></a>';
+	$output .= '<a href="'.  bloginfo("rss_url") . '" class="icon icon-rss" target="_blank"></a>';
+	$output .= '</div>';
+
+    return $output;
+}
+add_shortcode( 'socials', 'socials_func' );
+ ?>
