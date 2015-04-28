6bf910c8dcbea6c60f00f4f0a6153c3ac3826e3c
franszo <josh@kishandchips.com>
2015-04-28 11:00:24 +0100

    review by Med
    Signed-off-by: franszo <josh@kishandchips.com>

--- inc/default/shortcodes.php
+++ inc/default/shortcodes.php
@@ -2,9 +2,9 @@
 function socials_func( ) {
 
 	$output = '<div class="socials">';
-	$output .= '<a href="http://www.facebook.com/pages/The-Bank/277573288939381" target="_blank" class="icon icon-facebook"></a>';
-	$output .= '<a href="http://twitter.com/#!/TheBankAgency" class="icon icon-twitter" target="_blank"></a>';
 	$output .= '<a href="http://youtube.com/thebank" class="icon icon-youtube" target="_blank"></a>';
+	$output .= '<a href="http://twitter.com/#!/TheBankAgency" class="icon icon-twitter" target="_blank"></a>';
+	$output .= '<a href="http://www.facebook.com/pages/The-Bank/277573288939381" target="_blank" class="icon icon-facebook"></a>';
 	$output .= '</div>';
 
     return $output;
