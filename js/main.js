b648d4739d61cd302119c1c312ea712eaef496be
franszo <josh@kishandchips.com>
2015-05-05 18:34:30 +0100

    changes
    Signed-off-by: franszo <josh@kishandchips.com>

--- js/main.js
+++ js/main.js
@@ -112,7 +112,9 @@
 				    $(".scroll").click(function(event) {
 				    	event.preventDefault();
 				    	$('html,body').animate( { scrollTop: ($(this.hash).offset().top) - 160 } , 1000);
-				    });				    
+				    });	
+
+				    $('body').addClass('subnav');			    
 			    }								
 			}
 		},
